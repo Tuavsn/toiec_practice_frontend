6921ee8442f0d4477f2f463b1f57a0fc90aab55f
trhanhtu <tr.ho.anh.tu@gmail.com>
2024-10-05 22:38:36 +0700

    temp page for router admin

---
+++ src/pages/TempAdminPage.tsx
@@ -0,0 +1,14 @@
+import { Card } from "primereact/card";
+import React from "react";
+
+export default function TempAdminPage(text: string) {
+
+    return (
+        <React.Fragment>
+            <Card className="my-2">
+                <h1>Đây là trang {text}</h1>
+            </Card>
+        </React.Fragment>
+
+    )
+}
