8a884eaa518686b9a286e15edee25c0ac72e1d46
trhanhtu <tr.ho.anh.tu@gmail.com>
2024-10-05 22:27:53 +0700

    resolve conflict

--- src/components/Admin/LeftSideBar/LeftNavigationBar.tsx
+++ src/components/Admin/LeftSideBar/LeftNavigationBar.tsx
@@ -5,34 +5,34 @@ export default function LeftNavigationBar() {
     const navigate = useNavigate(); // Use the hook to get the navigate function
 
     const MenuItems = [
-        { 
-            label: 'Thống kê', 
+        {
+            label: 'Thống kê',
             items: [
-                { label: 'Thống kê bài làm', icon: 'pi pi-chart-pie', command: () => navigate('/dashboard/statistics') },
-                { label: 'Thống kê tài khoản', icon: 'pi pi-users', command: () => navigate('/dashboard/statistics/account') },
+                { label: 'Thống kê bài làm', icon: 'pi pi-chart-pie', command: () => navigate('/dashboard/test-analyst') },
+                { label: 'Thống kê tài khoản', icon: 'pi pi-users', command: () => navigate('/dashboard/user-analyst') },
             ]
         },
-        { 
-            label: 'Quản lý đề thi', 
+        {
+            label: 'Quản lý đề thi',
             items: [
-                { label: 'Quản lý bộ đề', icon: 'pi pi-folder', command: () => navigate('/dashboard/manage-categories') },
-                { label: 'Quản lý đề thi', icon: 'pi pi-file', command: () => navigate('/dashboard/manage-tests') },
+                { label: 'Quản lý bộ đề', icon: 'pi pi-folder', command: () => navigate('/dashboard/category') },
+                { label: 'Quản lý đề thi', icon: 'pi pi-file', command: () => navigate('/dashboard/test') },
             ]
         },
-        { 
-            label: 'Quản lý người dùng', 
+        {
+            label: 'Quản lý người dùng',
             items: [
-                { label: 'Quản lý tài khoản', icon: 'pi pi-user', command: () => navigate('/dashboard/manage-users') },
-                { label: 'Gửi thông báo', icon: 'pi pi-bell', command: () => navigate('/dashboard/send-notifications') },
-                { label: 'Quản lý chatbox', icon: 'pi pi-inbox', command: () => navigate('/dashboard/manage-chatbox') },
+                { label: 'Quản lý tài khoản', icon: 'pi pi-user', command: () => navigate('/dashboard/account') },
+                { label: 'Gửi thông báo', icon: 'pi pi-bell', command: () => navigate('/dashboard/notify') },
+                { label: 'Quản lý chatbox', icon: 'pi pi-inbox', command: () => navigate('/dashboard/chat') },
             ]
         },
     ];
 
     return (
         <div>
-            <Menu 
-                model={MenuItems} 
+            <Menu
+                model={MenuItems}
                 className="w-full"
             />
         </div>
